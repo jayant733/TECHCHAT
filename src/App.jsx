428abe70013a182e669557ac1f26cb73704bf0b0
Jayant Sharma <jayant.sharma2023@vitstudent.ac.in>
2024-12-21 09:43:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Header from './COMPONENTS/header'
+function App() {
+  
+
+  return (
+    <>
+    THIS IS THE REACT APP
+    <Header/>
+    </>
+  )
+}
+
+export default App
